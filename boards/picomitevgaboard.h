0e282c45707ed6bdf441c5953a0e874d8f0db7fe
ikjordan <iankjordan@gmail.com>
2024-04-07 18:14:27 +0100

    Support Waveshare pizero

--- boards/picomitevgaboard.h
+++ boards/picomitevgaboard.h
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 // -----------------------------------------------------
 // NOTE: THIS HEADER IS ALSO INCLUDED BY ASSEMBLER SO
 //       SHOULD ONLY CONSIST OF PREPROCESSOR DIRECTIVES
